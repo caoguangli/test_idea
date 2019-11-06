071bb14eb718f5fc751f2a126246eb59b0a5ed6c
caoguangli <3210737553@qq.com>
2019-11-06 21:47:17 +0800

    提交到本地仓库

---
+++ ssm/src/com/cgl/Demo.java
@@ -0,0 +1,13 @@
+package com.cgl;
+
+/**
+ * @author caoguangli
+ * @site www.xiaomage.com
+ * @company
+ * @create  2019-11-06 21:40
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("idea git 创建");
+    }
+}
